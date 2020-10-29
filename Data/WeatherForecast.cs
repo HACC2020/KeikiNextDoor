7c2433094139edc7fcf5689e9f84ebabe4247086
Sean Harding <seanmaika@hotmail.com>
2020-10-28 21:10:41 -1000

    Style: Configure brackets, spacing, and separation

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace OhanaSupport.Data
-{
-    public class WeatherForecast
-    {
+namespace OhanaSupport.Data {
+    public class WeatherForecast {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
