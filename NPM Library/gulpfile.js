021b9655ef7cc24dd492ed8561eec8cfda2b58a6
Sean Harding <seanmaika@hotmail.com>
2020-10-29 23:58:51 -1000

    Feat: Add gulp-rename and configure to tailwind.css

--- NPM Library/gulpfile.js
+++ NPM Library/gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp');
 
 gulp.task('css', () => {
-  const postcss = require('gulp-postcss');
+    const postcss = require('gulp-postcss');
+    const rename = require('gulp-rename');
 
-  return gulp.src('styles.css')
-    .pipe(postcss([
-      require('precss'),
-      require('tailwindcss'),
-      require('autoprefixer')
-    ]))
-    .pipe(gulp.dest('../wwwroot/css/tailwind.css'));
+    return gulp.src('styles.css')
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
+        .pipe(rename('tailwind.css'))
+        .pipe(gulp.dest('../wwwroot/css/'));
 });
